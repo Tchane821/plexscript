29105e4ac5370cabfa8aa519594f5b6a290ebf4d
Martin <googldu59@gmail.com>
2022-04-11 15:10:42 +0200

    End of file_upper script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# plexscript
+# Script PLEX
+
+### File Upper
+
+*Description* :  
+Script permetant de remonter les fichiers viedo d'une arboressence dans un dossier unique.  
+Utilise pour cela l'extension du fichier et ne remonte que les fichier en .mp4, .mkv, .avi
--- file_upper.py
+++ file_upper.py
@@ -1,19 +1,69 @@
 import os
+import sys
 from os.path import isdir
 import glob
 
-#saisie
-racine = input("Entrez le chemin de la racine des fichies a remonter\n")
+racine = ""
+destination = ""
+
+if len(sys.argv) == 3:
+    racine = sys.argv[1]
+    destination = sys.argv[2]
+    print(f"Log: Argument: root: {sys.argv[1]} destination: {sys.argv[2]}")
+else:
+    # saisie
+    racine = input("Entrez le chemin de la racine des fichies a remonter\n\t-> ")
+    destination = input("Entrez le nom du dossier de destination\n\t-> ")
+
+if not isdir(destination):
+    try:
+        os.mkdir(destination)
+    except FileNotFoundError:
+        print("Error : The path to destination is wrong")
+        exit(1)
+
 if not isdir(racine):
-    print("Erreur dans la saisie du dossier racine\n")
-    exit(1)
+    print("Error : the root argument is't not correct\n")
+    exit(2)
 
-ndr = input("Entrez le nom du dossier de destination\n") # Nom de Dossier de Reception
-if not isdir(ndr):
-    os.mkdir(ndr)
+# extraction
+path_to_file_list = glob.glob(racine + "/**", recursive=True)
 
-#extraction
-path_to_file_list = glob.glob(racine + '*mp4 | *mkv | *avi' )
-
+# Filter video only
+path_to_file_video_list = []
 for path_to_file in path_to_file_list:
-    print(path_to_file)
+
+    if ".mkv" in path_to_file or ".avi" in path_to_file or ".mp4" in path_to_file:
+        path_to_file_video_list.append(path_to_file)
+        print(path_to_file)
+
+# Validation de déplacement
+print(f"Log: File video Found: {len(path_to_file_video_list)}\n"
+      f"Souaitez vous les déplacer vers {destination}\n"
+      f"(Yes | No)\n\t -> ", end='')
+rep = input()
+if rep.upper() == "NO" or rep.upper() == "N":
+    print("Log: end of script with no erreor")
+    exit(0)
+elif rep.upper() == "YES" or rep.upper() == "Y":
+    print("Log: Launch move")
+else:
+    print("Error: Validation error")
+    exit(3)
+
+# Move
+v = 0
+for f in path_to_file_video_list:
+    f_name = os.path.basename(f)
+    f_res = destination + "/" + f_name
+    print(f"File : {f}, move to {f_res}")
+    try:
+        os.rename(f, f_res)
+    except FileExistsError:
+        f_res = os.path.dirname(f_res) + "/" + str(v) + '-' + os.path.basename(f_res)
+        print(f"File {f_res} exit, so {f_res} rename to {f_res}")
+        os.rename(f, f_res)
+    v += 1
+
+print("Log: End of script with no error")
+exit(0)
